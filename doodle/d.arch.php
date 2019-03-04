8df1b2b1c60038c1b4041559d2715cfc4ed25620
F 2D <from2d@gmail.com>
2019-03-04 13:07:27 +0300

    Archive rewrite fixes.
    Replace negative dates with earliest neighbour.
    Keep placeholder posts with link.

--- doodle/d.arch.php
+++ doodle/d.arch.php
@@ -4,14 +4,33 @@ define(ARCH_SITE_NAME, 'Doodle Mutator');
 define(ARCH_DESCRIPTION, 'Archived thread.');
 define(ARCH_PIC_NOT_FOUND, '<img src="'.ROOTPRFX.PIC_404.'">');
 define(ARCH_PAT_HTML_TAG, '~<\w+("[^">]*"|[^>])*>~u');
+define(ARCH_PAT_PLACEHOLDER_TEXT, 'v\d|'.mb_escape_regex(NOR).'|'.mb_escape_regex(html_entity_decode(NOR)));
+define(ARCH_PAT_PLACEHOLDER, '
+	(?P<PlaceholderText>'.ARCH_PAT_PLACEHOLDER_TEXT.')
+	(?P<PlaceholderLink>
+		[\s()]*
+		<a\s+[^<]+</a>
+		[\s()]*
+	)?
+	(?P<PlaceholderComment>\s*?<!--.*-->)?
+');
+define(ARCH_PAT_POST_PLACEHOLDER, '~^'.ARCH_PAT_PLACEHOLDER.'$~uix');
+define(ARCH_PAT_POST_PLACEHOLDER_SPAN, '~^
+	<span\s+title="
+	(?P<Title>
+		(?P<Time>\d+)
+		:\s+
+		(?P<Task>[^">]*)
+	)
+	">
+	(?P<Text>'.ARCH_PAT_PLACEHOLDER_TEXT.')
+	</span>
+$~uix');
 define(ARCH_PAT_POST, '~^
 	(?P<Date>[^\t]*)
 	(?P<Post>\t[^\t]*\t
 		(?:
-			(?P<Placeholder>
-				(?P<PlaceholderText>v\d|'.mb_escape_regex(NOR).')
-				(?:<!--.*-->)?
-			)
+			(?P<Placeholder>'.ARCH_PAT_PLACEHOLDER.')
 		|	(?P<Image><(?:a|img)\s.+)
 		|	(?P<Text>.+)
 		)
@@ -109,6 +128,7 @@ function data_archive_put_thumb($src, $dest, $xMax = 0, $yMax = 0) {
 	return imagePNG($data['imgdata'], $dest);
 }
 
+function data_archive_get_post_date($line) {return intval(mb_substr_before($line, ','));}
 function data_archive_get_post_pic_info($img_html, $check_file = 1) {
 	$a = mb_split($b = '>', $img_html);
 
@@ -121,37 +141,110 @@ function data_archive_get_post_pic_info($img_html, $check_file = 1) {
 	return $a;
 }
 
-function data_archive_get_fixed_content_line($line) {
-	global $data_archive_re_params;
+function data_archive_fix_image_path($post_content) {
+	foreach (array(
+		'<img src="'
+	,	'<a href="'
+	) as $i) {
+		$x = mb_split($i, $post_content);
+		$post_content = array_shift($x);
+		foreach ($x as $y) {
+			$src = mb_substr($y, 0, $q = mb_strpos($y, '"'));	//* <- web path
+			$n = mb_substr($src, mb_strrpos_after($src, '/'));	//* <- pic filename
+			$post_content .= $i.get_pic_url($n).mb_substr($y, $q);	//* <- new web path
+		}
+	}
+
+	return $post_content;
+}
+
+function data_archive_fix_image_html_and_meta($post_content, $recheck = false) {
+	if (!$recheck) $recheck = array();
+
+	$a = data_archive_get_post_pic_info($post_content, $recheck['hash'] ? 2 : 1);
+
+	$csv = (
+		($full_res = $a['full_res'])
+	?	"$full_res[0]*$full_res[1], $a[full_bytes_f], $a[full_bytes] B, 0x$a[crc32]"
+	:	implode(', ', mb_split_filter($a['csv'], '[;,]\\s*'))
+	);
+
+	if ($full_res && $full_res[0] > DRAW_PREVIEW_WIDTH) {
+		$a['resized_path'] = $post_content = get_pic_resized_path($a['rel_path']);
+		$a['resized_url'] = get_pic_url($post_content);
+		if (is_file($post_content)) {
+			$a['resized_res'] = getImageSize($post_content);
+		}
+		$post_content = '<a href="'.$a['full_url'].'">'
+		.	'<img src="'.$a['resized_url'].(
+				($resized_res = $a['resized_res'])
+				? '" width="'.$resized_res[0].'" height="'.$resized_res[1]
+				: ''
+			).'">'
+		.'</a>';
+	} else {
+		$post_content = '<img src="'.$a['full_url'].(
+			$full_res
+			? '" width="'.$full_res[0].'" height="'.$full_res[1]
+			: ''
+		).'">';
+	}
+	if ($csv) $post_content .= "; $csv";
+	if ($recheck['exists'] && !is_file($a['rel_path'])) {
+		$post_content = "<!--$post_content-->".ARCH_PIC_NOT_FOUND;
+	}
+
+	return $post_content;
+}
+
+function data_archive_get_post_fixed_values($line) {
+	global $data_archive_rewrite_params;
+
 	$sep = '	';
 	$tab = mb_split($sep, $line);
 
-//* timestamp:
+	$is_post_with_pic = (count($tab) > 3);
+
+//* date:
+
 	$t = $tab[0];
-	$t = (
-		preg_match('~\sdata-t=[\'"](\d+)~i', $t, $match)
-		? intval($match[1])
+	$post_date_int = intval(
+		preg_match('~\s+data-timestamp=[\'"](\d+)~i', $t, $match)
+		? $match[1]
 		: strtotime($t)
 	) ?: intval($t);
 
-	if (is_array($date = &$data_archive_re_params['date'])) {
-		if (!$date['min'] || $date['min'] > $t) $date['min'] = $t;
-		if (!$date['max'] || $date['max'] < $t) $date['max'] = $t;
+//* text placeholder:
+
+	$post_content = $tab[2];
+	if (
+		!$is_post_with_pic
+	&&	preg_match(ARCH_PAT_POST_PLACEHOLDER_SPAN, $post_content, $match)
+	) {
+		$post_content = (
+			strlen($match['Time']) && strlen($match['Task'])
+			? "$match[Text]<!-- $match[Title] -->"
+			: "$match[Text]"
+		);
 	}
 
-	$tab[0] = $t.','.date(DATE_ATOM, $t);
+	$is_post_placeholder = (
+		!$is_post_with_pic
+	&&	preg_match(ARCH_PAT_POST_PLACEHOLDER, $post_content)
+	);
 
-//* image/link:
-	$recheck = $data_archive_re_params['recheck_img'] ?: array();
-	$post_with_pic = (count($tab) > 3);
-	$p = $tab[2];
+//* image + optional link:
+
+	$recheck = $data_archive_rewrite_params['recheck_img'] ?: array();
+
 	if (
-		$post_with_pic
-	&&	false !== mb_strpos($p, '<img')
+		$is_post_with_pic
+	&&	false !== mb_strpos($post_content, '<img')
+	&&	($post_content = data_archive_fix_image_path($post_content))
 	&&	(
 			count(array_filter($recheck))
 		||	!(
-				preg_match(ARCH_PAT_POST_PIC, $p, $match)
+				preg_match(ARCH_PAT_POST_PIC, $post_content, $match)
 			&&	($csv = $match['csv'])
 			&&	preg_match(PAT_POST_PIC_CRC32, $csv)
 			&&	preg_match(PAT_POST_PIC_BYTES, $csv)
@@ -159,104 +252,172 @@ function data_archive_get_fixed_content_line($line) {
 			)
 		)
 	) {
-		$a = data_archive_get_post_pic_info($p, $recheck['hash'] ? 2 : 1);
-		$csv = (
-			($full_res = $a['full_res'])
-		?	"$full_res[0]*$full_res[1], $a[full_bytes_f], $a[full_bytes] B, 0x$a[crc32]"
-		:	implode(', ', mb_split_filter($a['csv'], '[;,]\\s*'))
-		);
-		if ($full_res && $full_res[0] > DRAW_PREVIEW_WIDTH) {
-			$a['resized_path'] = $p = get_pic_resized_path($a['rel_path']);
-			$a['resized_url'] = get_pic_url($p);
-			if (is_file($p)) {
-				$a['resized_res'] = getImageSize($p);
-			}
-			$p = '<a href="'.$a['full_url'].'">'
-			.	'<img src="'.$a['resized_url'].(
-					($resized_res = $a['resized_res'])
-					? '" width="'.$resized_res[0].'" height="'.$resized_res[1]
-					: ''
-				).'">'
-			.'</a>';
-		} else {
-			$p = '<img src="'.$a['full_url'].(
-				$full_res
-				? '" width="'.$full_res[0].'" height="'.$full_res[1]
-				: ''
-			).'">';
-		}
-		if ($csv) $p .= "; $csv";
-		if ($recheck['exists'] && !is_file($a['rel_path'])) {
-			$p = "<!--$p-->".ARCH_PIC_NOT_FOUND;
-		}
-		$tab[2] = $p;
-	} else
-	if (
-		!$post_with_pic
-	&&	preg_match('~^<span title="(?P<Title>(?P<Time>\d+):\s+(?P<Task>[^>]*))">(?P<Text>'.NOR.')</span>$~iu', $p, $match)
-	) {
-		$tab[2] = (
-			strlen($match['Time']) && strlen($match['Task'])
-			? "$match[Text]<!-- $match[Title] -->"
-			: "$match[Text]"
-		);
+		$post_content = data_archive_fix_image_html_and_meta($post_content, $recheck);
 	}
+
+	return array(
+		'tab' => $tab
+	,	'post_content' => $post_content
+	,	'post_date_int' => $post_date_int
+	,	'is_post_with_pic' => $is_post_with_pic
+	,	'is_post_placeholder' => $is_post_placeholder
+	);
+}
+
+function data_archive_get_post_fixed_lines($post) {
+	global $data_archive_rewrite_params;
+
+	$sep = '	';
+
+	extract($post);
+
+//* store min/max dates:
+
+	if (is_array($date_span = &$data_archive_rewrite_params['date_span'])) {
+		if (!$date_span['min'] || $date_span['min'] > $post_date_int) $date_span['min'] = $post_date_int;
+		if (!$date_span['max'] || $date_span['max'] < $post_date_int) $date_span['max'] = $post_date_int;
+	}
+
+//* store post text for meta tags:
+
+	if (
+		!$is_post_with_pic
+	&&	is_array($meta = &$data_archive_rewrite_params['meta'])
+	) {
+		$k = ($is_post_placeholder ? 'placeholder' : 'description');
+		$j = $k.'-date';
+
+		if (
+			(
+				!$meta[$k]
+			||	$meta[$j] > $post_date_int
+			)
+		&&	($post_text = preg_replace(ARCH_PAT_HTML_TAG, '', $post_content))
+		) {
+			$meta[$k] = $post_text;
+			$meta[$j] = $post_date_int;
+		}
+	}
+
+	$tab[0] = $post_date_int.','.date(DATE_ATOM, $post_date_int);
+	$tab[2] = $post_content;
 
 	return implode($sep, $tab);
 }
 
+function data_archive_sort_post_by_date($a, $b) {
+	return (
+		($a['post_date_int'] <=> $b['post_date_int'])
+	?:	strcmp($a['post_content'], $b['post_content'])
+	);
+}
+
+function data_archive_fix_post_date(&$posts, $i, $increment = false) {
+	$old_date = $post_date = $posts[$i]['post_date_int'];
+
+	if ($post_date <= 0) {
+		$post_date = (
+			(
+				$increment
+				? ($i > 0)
+				: ($i == count($posts) - 1)
+			)
+			? ($posts[$i-1]['post_date_int'] + 1)
+			: ($posts[$i+1]['post_date_int'] - 1)
+		);
+	}
+
+	if ($post_date > 0) {
+		$same_date_count = 0;
+
+		foreach ($posts as $post) if ($post_date === $post['post_date_int']) {
+			++$same_date_count;
+		}
+
+		if ($same_date_count > 1) {
+			if ($increment) {
+				++$post_date;
+			} else {
+				--$post_date;
+			}
+		}
+
+		$changed = ($old_date !== $post_date);
+
+		if ($changed) {
+			$posts[$i]['post_date_int'] = $post_date;
+		}
+	}
+
+	return intval($changed);
+}
+
 function data_archive_is_a_content_line($line) {return (false !== mb_strpos($line, '	'));}
 function data_archive_get_page_html($room, $num, $tsv) {
-	global $data_archive_re_params, $cfg_langs, $cfg_link_canon;
-	if (!is_array($data_archive_re_params)) $data_archive_re_params = array();
-	$date = (array(
+	global $data_archive_rewrite_params, $cfg_langs, $cfg_link_canon;
+
+	if (!is_array($data_archive_re_params)) {
+		$data_archive_re_params = array();
+	}
+	$meta = array();
+	$date_span = array(
 		'min' => 0
 	,	'max' => 0
-	));
-	$data_archive_re_params['date'] = &$date;
+	);
+	$data_archive_rewrite_params['date_span'] = &$date_span;
+	$data_archive_rewrite_params['meta'] = &$meta;
+
 	if ($num <= 0) return false;
 
 	$p = $num-1;
 	$n = $num+1;
 
+//* deconstruct text block into post values:
+
 	$lines = mb_split_filter(trim(fix_encoding($tsv)), NL);
 	$lines = array_filter($lines, 'data_archive_is_a_content_line');
-	$dates = array_map('intval', $lines);
+	$posts = array_map('data_archive_get_post_fixed_values', $lines);
 
-	$post_text_types = array(
-		'Text' => 'desciption'
-	// ,	'Placeholder' => 'placeholder'
+//* fix non-positive date values without sorting, to keep paired posts together:
+
+	$tries = 0;
+	$max_tries = TRD_MAX_POSTS * 2;
+
+	do {
+		$changed = 0;
+
+		foreach ($posts as $i => $post) if ($post['is_post_placeholder']) {
+			$changed += data_archive_fix_post_date($posts, $i);
+		}
+
+		foreach ($posts as $i => $post) if (!$post['is_post_placeholder']) {
+			$changed += data_archive_fix_post_date($posts, $i, true);
+		}
+	} while (
+		$changed
+	&&	(++$tries < $max_tries)
 	);
 
-	foreach ($dates as $i => $no_task_post_date) if (preg_match(ARCH_PAT_POST, $lines[$i], $match)) {
-		if ($match['Placeholder']) {
-			$same_date_count = 0;
-			foreach ($dates as $post_date) if ($post_date === $no_task_post_date) {
-				++$same_date_count;
-			}
-			if ($same_date_count > 1) {
-				$lines[$i] = ($no_task_post_date - 1).$match['Post'];
-			}
-		} else
-		foreach ($post_text_types as $m_k => $v_k) if ($v = $match[$m_k]) {
-			if (
-				!$$v_k
-			&&	($v = preg_replace(ARCH_PAT_HTML_TAG, '', $v))
-			) {
-				$$v_k = $v;
-			}
-			break;
-		}
-	}
+//* sort by fixed date values:
 
-	$lines = array_map('data_archive_get_fixed_content_line', $lines);
-	sort($lines);
+	usort($posts, 'data_archive_sort_post_by_date');
+
+//* reconstruct posts into text block:
+
+	$lines = array_map('data_archive_get_post_fixed_lines', $posts);
 	$tsv = NL.implode(NL, $lines);
+
+	if (!strlen(trim($tsv))) {
+		return false;
+	}
 
 	$full_link_prefix = rtrim($cfg_link_canon, './');
 	$link_here = ROOTPRFX.DIR_ARCH."$room/$num".PAGE_EXT;
 	$thumbnail = ROOTPRFX.DIR_ARCH."$room/".DIR_THUMB.$num.THUMB_EXT;
-	$desciption = date('Y-m-d', $date['min']).'. '.($desciption ?: "$room - $num");
+
+	$date_min = date('Y-m-d', $date_span['min']);
+	$description = $meta['description'] ?: "$room - $num";
+	$description = "$date_min. $description";
 
 	return get_template_page(
 		array(
@@ -271,10 +432,10 @@ function data_archive_get_page_html($room, $num, $tsv) {
 				'og:type'  => 'article'
 			,	'og:url'   => $full_link_prefix.$link_here
 			,	'og:image' => $full_link_prefix.$thumbnail
-			,	'og:title' => $desciption
+			,	'og:title' => $description
 			,	'og:site_name' => ARCH_SITE_NAME
 			)
-		,	'body' => get_date_class($date['min'], $date['max'])
+		,	'body' => get_date_class($date_span['min'], $date_span['max'])
 		,	'task' => ($p > 0 ? '<a href="'.$p.PAGE_EXT.'" title="previous">'.$num.'</a>' : $num)
 		,	'content' => $tsv
 		,	'js' => array('capture' => 1, 'arch' => 1)
@@ -303,7 +464,8 @@ function data_archive_full_threads($threads) {
 	foreach ($threads as $a) {
 		++$i;
 		if (
-			file_put_contents("$d$i$e", data_archive_get_page_html($room, $i, $a['content']))
+			($file_content = data_archive_get_page_html($room, $i, $a['content']))
+		&&	file_put_contents("$d$i$e", $file_content)
 	//	&&	unlink($a['name'])
 		&&	data_del_thread($a['name'])	//* <- clean up comments, etc
 		) {
@@ -336,16 +498,11 @@ function data_archive_full_threads($threads) {
 }
 
 function data_archive_rewrite($params = false) {
-	global $data_archive_re_params, $date_classes;
-	$data_archive_re_params = (array)$params;
+	global $data_archive_rewrite_params, $date_classes;
+	$data_archive_rewrite_params = (array)$params;
 	$a = 0;
 	$d = DIR_ARCH;
 	$elen = -strlen(PAGE_EXT);
-	$img_src = array(
-		'<img src="'
-	,	'<a href="'
-	);
-
 	data_lock($lk = LK_ARCH.$r, true);
 	foreach (get_dir_rooms($d, '', F_NATSORT) as $room) {
 		$t = 0;
@@ -354,17 +511,7 @@ function data_archive_rewrite($params = false) {
 		&&	is_file($f = "$dr/$fn")
 		&&	preg_match(PAT_CONTENT, $old = file_get_contents($f), $match)
 		) {
-			$new = $match['content'];
-			foreach ($img_src as $i) {
-				$x = mb_split($i, $new);
-				$new = array_shift($x);
-				foreach ($x as $y) {
-					$src = mb_substr($y, 0, $q = mb_strpos($y, '"'));	//* <- web path
-					$n = mb_substr($src, mb_strrpos_after($src, '/'));	//* <- pic filename
-					$new .= $i.get_pic_url($n).mb_substr($y, $q);		//* <- new web path
-				}
-			}
-			$new = data_archive_get_page_html($room, intval($fn), $new);
+			$new = data_archive_get_page_html($room, intval($fn), $match['content']);
 			if ($old == $new) $x = 'same';
 			else if (!$new) $x = 'error';
 			else {
@@ -373,12 +520,12 @@ function data_archive_rewrite($params = false) {
 				if (!unlink($x)) $x = 'delete old failed'; else
 				$x = strlen($old)." => $sz bytes";
 			}
-			$date = array();
-			foreach ($data_archive_re_params['date'] as $k => $v) {
-				$date[$k] = date(TIMESTAMP, $v);
+			$date_span = array();
+			foreach ($data_archive_rewrite_params['date_span'] as $k => $v) {
+				$date_span[$k] = date(TIMESTAMP, $v);
 			}
 			$dc = ($date_classes ? '	'.implode(' ', $date_classes) : '');
-			$text_report .= NL."$f	$x	$date[min] - $date[max]$dc";
+			$text_report .= NL."$f	$x	$date_span[min] - $date_span[max]$dc";
 			++$t;
 		}
 		++$a;
